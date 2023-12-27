110ae6feb961b3c3feb1ee53d38e205081fa68e4
mansi2020 <sorathiyamansi11@gmail.com>
2023-12-27 05:44:13 +0530

    add changes

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,18 +1,15 @@
-import React, { useEffect } from 'react'
-import './card.css';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import "./card.css";
 
 const Card = (props) => {
-
-  useEffect(()=>{
-    Aos.init({duration:500});
-  },[]);
   return (
-    <div className='card_container' style={{backgroundColor:props.color}} data-aos="zoom-in">
+    <div
+      className="card_container"
+      style={{ backgroundColor: props.color }}
+      data-aos="zoom-in"
+    >
       <h1>{props.title}</h1>
       <span>{props.subtitle}</span>
     </div>
-  )
-}
-export default Card
+  );
+};
+export default Card;
